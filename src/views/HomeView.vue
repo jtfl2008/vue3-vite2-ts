6cfd3085c34bef02e3643ca6b5822a74137df1e9
dongjianglong <dongjianglong@csftgroup.com>
2022-03-31 17:02:25 +0800

    file📦:  删除无用文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+  defineProps<{
+    msg: string
+  }>()
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
 </template>
