7af61496091c2a8040dd9e32dea8b4e88bbfc964
dongjianglong <dongjianglong@csftgroup.com>
2022-03-31 19:39:49 +0800

    feat✨:  添加别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-import { fileURLToPath, URL } from 'url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -9,7 +8,9 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@assets': resolve(__dirname, './src/assets'),
     }
   }
 })
