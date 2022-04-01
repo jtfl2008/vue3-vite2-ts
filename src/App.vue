b9dc4552cdec131122cca0bd8b6e41df705bb3a0
dongjianglong <dongjianglong@csftgroup.com>
2022-04-01 18:46:43 +0800

    feat✨:  引入stylelint

--- src/App.vue
+++ src/App.vue
@@ -19,29 +19,34 @@
 
 <style lang="scss">
   @import '@/assets/base.css';
+
   .app-container {
     margin: 0 auto;
     width: 1200px;
   }
+
   a,
   .green {
     text-decoration: none;
-    color: hsla(160, 100%, 37%, 1);
+    color: hsl(160deg 100% 37% / 100%);
     transition: 0.4s;
   }
 
   nav {
+    margin-top: 2rem;
     width: 100%;
     font-size: 12px;
     text-align: center;
-    margin-top: 2rem;
+
     a {
       display: inline-block;
       padding: 0 1rem;
     }
+
     a.router-link-exact-active {
       color: var(--color-text);
     }
+
     a.router-link-exact-active:hover {
       background-color: transparent;
     }
